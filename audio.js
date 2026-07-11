906adeffb9682a27948b27d537d3cd6266d6ddd1
Rudy-2012 <rudik.movsisyan@tumo.org>
2026-07-11 15:48:34 +0400

    commit 2

---
+++ audio.js
@@ -0,0 +1,45 @@
+let data = {
+    title :[
+        "Aram Asatryan Asemm Te Chasem",
+        "Aram Asatryan Draxti Partez",
+        "Artash Asatryan Miami",
+        "Artsakh is Armenia 333",
+        "Ashot Hovsepyan Kyanqi Karusel",
+        "Inna Hot",    
+        "Inna Sun is UP",
+        "SlaVVo Ay Leto Leto",
+        "Tatul Avoyan Ampern elan qula qula",
+
+    ],
+    song : [
+        "music/Aram Asatryan Asemm Te Chasem.mp3",
+        "music/Aram Asatryan Draxti Partez.mp3",
+        "music/Artash Asatryan Miami.mp3",
+        "music/Artsakh is Armenia 333.mp3",
+        "music/Ashot Hovsepyan Kyanqi Karusel.mp3",
+        "music/Inna Hot.mp3",    
+        "music/Inna Sun is UP.mp3",
+        "music/SlaVVo Ay Leto Leto.mp3",
+        "music/Tatul Avoyan Ampern elan qula qula.mp3",
+
+    ],
+
+    poster :[
+          "https://64.media.tumblr.com/c80cc57fe20c6b1d8810fb8fb10a52e2/tumblr_n0fwemRUKU1trsyieo1_500.gifv",
+          "https://64.media.tumblr.com/303c713aac924ac147fd047f6cda7931/tumblr_n0dsroizYs1trsyieo1_500.gifv",
+          "https://viberate-upload.ams3.cdn.digitaloceanspaces.com/prod/entity/artist/artash-asatryan-vlrXE",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTGJ4djC9rLkRAd5xW6oX-MQF0iSvpA-RVj1z0Am34lw&s=10",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQByMTpAoHxis-zBAIDKczoFSuyzuEdvL8w9VdLbyGkpA&s=10",
+          "https://i.makeagif.com/media/3-15-2017/zuIlPB.gif",
+          "https://media.tenor.com/t35n21GSVPMAAAAM/inna-cola-song.gif",
+          "https://i1.sndcdn.com/artworks-000019138751-l4qvmw-t1080x1080.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQY8vd6mDX9YIbwJRPg-5uA3aV9BWjBm6XnfrDoumW3b-t1HaDhmN3pJU&s=10",
+          "https://i.ytimg.com/vi/nE2b3sJ-Ejo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA2ZSpFOh2ov2LxDA40R_2xgfT3hw",
+          
+          
+
+    ],
+    
+
+
+}
