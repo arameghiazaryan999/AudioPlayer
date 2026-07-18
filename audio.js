5508ee59fbb4e85337799f25415082f0a85ff589
narekhuneyan-crypto <narek.huneyan@tumo.org>
2026-07-18 16:00:42 +0400

    final upload

--- audio.js
+++ audio.js
@@ -12,16 +12,13 @@ let data = {
 
     ],
     song : [
-        "music/Aram Asatryan Asemm Te Chasem.mp3",
-        "music/Aram Asatryan Draxti Partez.mp3",
-        "music/Artash Asatryan Miami.mp3",
-        "music/Lyov 333 Feat.mp3",
+        "music/Aram Asatryan - Asem te chasem (Я кайфую от этой песни).mp3",
+        "music/Aram Asatryan Draxti Peri.mp3",
+        "music/Artash Asatryan - Miami (1).mp3",
+        "music/Artsakh Is Armenia  333 (Feat. Lyov).mp3",
         "music/Ashot Hovsepyan Kyanqi Karusel.mp3",
-        "music/Inna Hot.mp3",    
-        "music/Inna Sun is UP.mp3",
-        "music/SlaVVo Ay Leto Leto.mp3",
-        "music/Tatul Avoyan Ampern elan qula qula.mp3",
-
+        "music/Inna - Caliente (Extended Version).mp3",    
+        "music/Inna Sun is UP.mp3"
     ],
 
     poster :[
@@ -46,51 +43,151 @@ let data = {
 
 
 }
-
-let song = new Audio()
 let currentSong = 0
 
-window.onload = function (){
+let song = new Audio()
+
+window.onload = function () {
     playSong()
 }
 
 
-
-
-
-function playSong(){
+function playSong() {
     song.src = data.song[currentSong]
     let songTitle = document.getElementById("songTitle")
-    songTitle.textContrnt = data.title[currentSong]
-    let img = document.getElementById("row1")
-    img.style.backgroundImige = "url(" + data.poster[currentSong] +")"
-    
-    let main = document.getElementById("main")
-    main.style.backgroundImige = "url(" + data.poster[currentSong] +")"
+    songTitle.textContent = data.title[currentSong]
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
     song.play()
-
-   
 }
 
 
-function playOrPauseSong(){
+function playOrPauseSong() {
     let play = document.getElementById("play")
 
-    if(song.paused){
-
+    if (song.paused) {
         song.play()
         play.src = "images/pause.png"
-    }else{
+    } else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
+    }
+}
 
 
+
+
+console.log(song);
+
+song.addEventListener("timeupdate", function () {
+
+    let fill = document.getElementsByClassName("fill")
+    let position = song.currentTime / song.duration
+    fill[0].style.marginLeft = position * 99 + "%"
+
+
+    convertTime(song.currentTime)
+    totalTime(Math.round(song.duration))
+
+    if (song.ended) {
+        next()
+    }
+})
+
+
+
+function convertTime(seconds) {
+    let currentTime = document.getElementsByClassName("currentTime")
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    currentTime[0].textContent = min + ":" + sec
     
+}
+
+
+function totalTime(seconds) {
+
+        console.log(song.duration);
+        let currentTime = document.getElementsByClassName("currentTime")
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+
+    currentTime[0].textContent += " / " + min + ":" + sec
+    
+}
+
+function prev() {
+    currentSong--
+    let play = document.getElementById("play")
+    if (currentSong < 0) {
+        currentSong = data.song.length - 1
     }
 
+    playSong()
+    play.src = "images/pause.png"
+}
+
+
+function  next(){
+      currentSong++
+
+
+      let play = document.getElementById("play")
+    if (currentSong == data.song.length) {
+        currentSong = 0
+    }
+
+    playSong()
+    play.src = "images/pause.png"
+
+}
 
 
 
+function  mute(){
+       let mute  = document.getElementById("mute")
+       
+       if(song.muted ){
+                    song.muted = false
+                    mute.src = "images/volume.png"
+       }else{
+                    song.muted  = true
+                    mute.src  = "images/volume-mute.png"
+       }
+       
+}
+
+
+function  decrease(){
+    let mute  = document.getElementById("mute")
+
+    song.volume -= 0.2
+
+    if(song.volume <= 0.1){
+                mute.src  = "images/volume-mute.png"
+    }
+}
+
+function increase(){
+    let mute  = document.getElementById("mute")
+
+    song.volume += 0.2
+
+    if(song.volume >= 0.2){
+        mute.src = "images/volume.png"
+    }
 }
 
 
